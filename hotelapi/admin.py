f807be0a3e572514bda84e15a57d36ac3c9a5b1f
ddssaa22012 <62133597+ddssaa22012@users.noreply.github.com>
2020-05-22 13:39:51 +0800

    Update admin.py

--- hotelapi/admin.py
+++ hotelapi/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from hotelapi.models import booking,user
 # Register your models here.
+class usersAdmin(admin.ModelAdmin):
+    list_display=('uid','created_time')
+admin.site.reister(user,usersAdmin):
+    list_display=('bid','roomtype','roomamount', 'datein', 'dateout' )
+admin.site.register(booking,bookingAdmin)
